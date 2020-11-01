d977fce014172737e879780b47e9c349e4da9007
a1930yn <a1930yn@aiit.ac.jp>
2020-11-01 21:37:41 +0900

    template updated.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,18 +12,31 @@
         <th>ExpectedAshFall_2</th>
         <th>LocalGovernment_2</th>
         <th>WarningCode_2</th>
+        <th>ExpectedAshFall_3</th>
+        <th>LocalGovernment_3</th>
+        <th>WarningCode_3</th>
+        <th>ExpectedAshFall_4</th>
+        <th>LocalGovernment_4</th>
+        <th>WarningCode_4</th>
       </tr>
       </thead>
         <tbody>
        <tr v-for="disaster in disasters" v-bind:key="disaster">
         <td> {{disaster.messageType}}</td>
-        <td> {{disaster.disasterCategory}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
-        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments}}</td>
+        <td> {{disaster.disasterCategory}}</td>   
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[0].expectedAshFall}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[0].localGovernment}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[0].warningCode}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[1].expectedAshFall}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[1].localGovernment}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[1].warningCode}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[2].expectedAshFall}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[2].localGovernment}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[2].warningCode}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[3].expectedAshFall}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[3].localGovernment}}</td>
+        <td> {{disaster.ashFallTimeWarningCodeLocalGovernments[3].warningCode}}</td>
+        <v-for/>
         </tr>
         </tbody>
       </table>
